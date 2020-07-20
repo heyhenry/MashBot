7bde0f3a9fa6c3031076e564428c7bc53255bad4
Nampa <henryyy3@hotmail.com>
2020-07-20 15:20:06 +0800

    Initial creation of MashBot, a discord bot.

---
+++ src/main/java/Mash.java
@@ -0,0 +1,12 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Mash {
+
+    public static void main(String[] args) throws Exception {
+
+        JDA jda = JDABuilder.createDefault("NzM0NjY4ODg3MDYwMzgxNjk2.XxVD6Q.46fFEvu-9i_YoqcWm4wRsmBj28k").build();
+
+    }
+
+}
